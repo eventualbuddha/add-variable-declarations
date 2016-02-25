4b677caaeed9371ad49944eb57b16fc8ce65b0cd
Brian Donovan <donovan@squareup.com>
2016-02-25 12:12:12 -0800

    Read babel config from .babelrc.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
 import babel from 'rollup-plugin-babel';
+import { readFileSync } from 'fs';
+
+let babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
+babelConfig.presets = babelConfig.presets.map(
+  preset => preset === 'es2015' ? 'es2015-rollup' : preset
+);
+babelConfig.babelrc = false;
 
 export default {
   entry: 'src/index.js',
-  plugins: [babel({
-    babelrc: false,
-    presets: ['es2015-rollup'],
-    plugins: ['syntax-flow', 'transform-flow-strip-types']
-  })]
+  plugins: [babel(babelConfig)]
 };
