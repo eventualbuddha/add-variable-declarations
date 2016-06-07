89ff91acacf9d4ce5f6852f92b22e4a5ec41f5df
Brian Donovan <me@brian-donovan.com>
2016-06-07 05:24:58 -0700

    Use babelrc-rollup to clean up the rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,7 @@
 import babel from 'rollup-plugin-babel';
-import { readFileSync } from 'fs';
-
-let babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
-babelConfig.presets = babelConfig.presets.map(
-  preset => preset === 'es2015' ? 'es2015-rollup' : preset
-);
-babelConfig.babelrc = false;
+import babelrc from 'babelrc-rollup';
 
 export default {
   entry: 'src/index.js',
-  plugins: [babel(babelConfig)]
+  plugins: [babel(babelrc())]
 };
