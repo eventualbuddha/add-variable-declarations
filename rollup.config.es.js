5c4ea0b8cd86880f9177f9ca86b6b53d31a2ea92
Brian Donovan <me@brian-donovan.com>
2016-06-07 05:29:02 -0700

    Use multiple targets in a single rollup config.

--- rollup.config.es.js
+++
@@ -1,6 +0,0 @@
-import config from './rollup.config';
-
-config.dest = 'dist/add-variable-declarations.es.js';
-config.format = 'es6';
-
-export default config;
--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,16 @@ import babelrc from 'babelrc-rollup';
 
 export default {
   entry: 'src/index.js',
-  plugins: [babel(babelrc())]
+  plugins: [babel(babelrc())],
+  targets: [
+    {
+      dest: 'dist/add-variable-declarations.es.js',
+      format: 'es6'
+    },
+    {
+      dest: 'dist/add-variable-declarations.umd.js',
+      format: 'umd',
+      moduleName: 'addVariableDeclarations'
+    }
+  ]
 };
