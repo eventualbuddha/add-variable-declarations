0204ffb62271925df4cbbec85593fd8652a6d469
Brian Donovan <me@brian-donovan.com>
2016-07-15 11:10:06 -0700

    chore: add a failing (skipped) test illustrating a bug
    https://github.com/decaffeinate/decaffeinate/issues/316

---
+++ test/fixtures/multiple-assignments-in-nested-scopes-hoists-to-shared-scope/config.js
@@ -0,0 +1,2 @@
+export const description = 'multiple assignments in nested scopes hoists to shared scope';
+export const skip = true;
---
+++ test/fixtures/multiple-assignments-in-nested-scopes-hoists-to-shared-scope/expected.js
@@ -0,0 +1,10 @@
+var r;
+if (a) {
+  if (b) {
+    r = 0;
+  } else {
+    r = 1;
+  }
+} else {
+  r = 2;
+}
