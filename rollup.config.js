43d713ecf20ea27fae7369e2ebce339ab321e9fa
Brian Donovan <me@brian-donovan.com>
2016-08-14 11:56:11 -0700

    chore: update dependencies (#9)

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 
+var external = Object.keys(require('./package.json').dependencies);
+
 export default {
   entry: 'src/index.js',
   plugins: [babel(babelrc())],
+  external: external,
   targets: [
     {
       dest: 'dist/add-variable-declarations.mjs',
-      format: 'es6'
+      format: 'es'
     },
     {
       dest: 'dist/add-variable-declarations.js',
